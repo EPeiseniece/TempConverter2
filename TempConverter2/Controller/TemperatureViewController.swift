2dc14d77797f060c133738433093f7681159dff6
EPeiseniece <87663601+EPeiseniece@users.noreply.github.com>
2021-08-11 13:19:43 +0300

    Outlets+ActionSetup

--- TempConverter2/Controller/TemperatureViewController.swift
+++ TempConverter2/Controller/TemperatureViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class TemperatureViewController: UIViewController {
 
+    @IBOutlet weak var celciusLabel: UILabel!
+    @IBOutlet weak var convertedTemperatureLabel: UILabel!
+    @IBOutlet weak var temperatureSegmentControl: UISegmentedControl!
+    
+    @IBOutlet weak var temperatureSlider: UISlider!{
+        didSet{
+            temperatureSlider.maximumValue = 100
+            temperatureSlider.minimumValue = 0
+            temperatureSlider.value = 0
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        convertedTemperatureLabel.text = "32 ºF"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sliderValueChanged(_ sender: Any) {
+    }
+    
+    @IBAction func segmentValueChanged(_ sender: UISegmentedControl) {
+    }
+    
+    
 }
 

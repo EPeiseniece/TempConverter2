bcef09c70a14f02413dd4bf0ff0646936fa2af65
EPeiseniece <87663601+EPeiseniece@users.noreply.github.com>
2021-08-11 14:03:08 +0300

    2 views

---
+++ TempConverter2/Controller/AppearanceViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AppearanceViewController.swift
+//  TempConverter2
+//
+//  Created by elina.peiseniece on 11/08/2021.
+//
+
+import UIKit
+
+class AppearanceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+  /*  func setLabelText(){
+        var text = "Unable to specify UI style"
+        if traitCollection.userInterfaceStyle == .dark{
+            text = "Dark Mode is On\n Go to settings if you like \n to change to light mode"
+        }else{
+            text = "Light Mode is On\n Go to settings if you like \n to change to dark mode"
+        }
+        textLabel.text = text
+       
+    }
+     */
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
